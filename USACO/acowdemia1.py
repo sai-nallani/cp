e513c0d2f665d109b0bba8926c9b27dc84f46f92
Sai Nallani <sai.s.nallani@gmail.com>
2023-02-23 19:06:49 -0500

    stuff

--- USACO/acowdemia1.py
+++ USACO/acowdemia1.py
@@ -1,6 +1,9 @@
+import sys
+
+# sys.stdin = open("in.in")
 n, l = map(int, input().split())
 c = list(map(int, input().split()))
-
+n = len(c)
 c.sort()
 
 
@@ -11,34 +14,41 @@ for i, ci in enumerate(c):
         if (ci >= max_h):
             max_h = ci
             max_h_index = i
+# adds one here probably
+if (l > 0):
+    # print(max_h, max_h_index)
 
-# print(max_h, max_h_index)
-
-new_h = max_h+1
-while True:
-    if (max_h_index > 0 and c[max_h_index] >= new_h):
-        max_h_index -= 1
-    else:
-        break
-
-# print(max_h_index)
-
-for _ in range(l):
-    if (max_h_index > 0):
-        c[max_h_index] += 1
-        max_h_index -= 1
-    else:
-        c[max_h_index] += 1
-        break
-
-
-# print(c)
-c.sort()
-for i, ci in enumerate(c):
-    if (n - i >= ci):
-        if (ci >= max_h):
-            max_h = ci
+    new_h = max_h+1
+    for i in range(n-1, -1, -1):
+        if (c[i] == max_h):
             max_h_index = i
+            break
+
+    # while True:
+    #     if (max_h_index > 0 and c[max_h_index] >= new_h):
+    #         max_h_index -= 1
+    #     else:
+    #         break
+
+    # print(max_h_index)
+
+    for _ in range(l):
+        if (max_h_index > 0):
+            c[max_h_index] += 1
+            max_h_index -= 1
+        else:
+            c[max_h_index] += 1
+            break
+
+
+    # print(c)
+    c.sort()
+    for i, ci in enumerate(c):
+        if (n - i >= ci):
+            if (ci >= max_h):
+                max_h = ci
+                max_h_index = i
+
 
 print(max_h)
 
